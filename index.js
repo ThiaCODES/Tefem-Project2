4a42ddf81f3322cced71bf892530a76c96fdf92f
ThiaCODES <moshoodfatia@gmail.com>
2024-11-12 02:22:53 +0100

    commit

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const unorderedlist = document.querySelector("ul");
-const navbtn = document.querySelector(".fa-bars");
-const bright = document.querySelector(".fa-moon");
+const unorderedlist = document.querySelector(".Links");
+const bright = document.querySelector("#bright");
 const container = document.querySelector(".container");
 const heading = document.querySelector("header");
 const links = document.querySelectorAll("a");
-
 const copyright = document.querySelector(".copyright");
+const sidenav = document.querySelector(".sidebar");
 bright.addEventListener("click", changeMode);
 function changeMode() {
   document.body.classList.toggle("darkmode");
+  bright.classList.toggle("fa-moon");
+  bright.classList.toggle("fa-sun");
   if (document.body.classList.contains("darkmode")) {
     container.style.color = "white";
+
     heading.style.backgroundColor = "black";
     copyright.classList.remove();
     copyright.classList.add("edited");
     links.forEach((item) => {
-      item.classList.add("link");
       item.style.color = "white";
     });
   } else {
@@ -31,18 +32,32 @@ function changeMode() {
   }
 }
 console.log(links);
-function activeState() {}
-links.forEach((item) => {
-  item.addEventListener("click", () => {
-    links.forEach((linkbtn) => {
-      linkbtn.classList.remove("active");
-      item.classList.add("active");
+
+//add active class
+
+function activeState() {
+  links.forEach((item) => {
+    item.addEventListener("click", () => {
+      unorderedlist.style.display = "none";
+      links.forEach((linkbtn) => {
+        linkbtn.classList.remove("active");
+        item.classList.add("active");
+      });
     });
   });
-});
-navbtn.addEventListener("click", showList);
+}
+activeState();
+
 function showList() {
   unorderedlist.classList.toggle("list");
   navbtn.classList.toggle("fa-xmark");
 }
 console.log(dish);
+
+function showBar() {
+  sidenav.style.display = "flex";
+}
+
+function closeBar() {
+  sidenav.style.display = "none";
+}
